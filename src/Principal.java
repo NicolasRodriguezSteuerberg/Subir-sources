268f8eac4d0d8e326712f373d81d6d9be64ad73c
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-10-14 09:05:33 +0200

    terminada version 1.o

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Principal {
+    /**
+     * @version 1.0
+     */
     public static void main(String[] args) {
-        System.out.println("pa ti mi cola");
+        JOptionPane.showMessageDialog(null,"Hola!");
+        JOptionPane.showMessageDialog(null,"Adios!");
+
     }
 }
